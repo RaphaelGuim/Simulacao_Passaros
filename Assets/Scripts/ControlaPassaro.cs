2fcf8e20c2c708d1e0fdfd8812e8fe3ffee13295
RaphaelGuim <guimaraesraphael0203@gmail.com>
2018-12-06 18:33:10 -0200

    Adicionando Arquivos do Simulador

---
+++ Assets/Scripts/ControlaPassaro.cs
@@ -0,0 +1,200 @@
+﻿using UnityEngine;
+
+public class ControlaPassaro : MonoBehaviour {
+
+	private Rigidbody rb;
+	public float maxSpeed;
+	private Controle controle;
+	private float AnguloVisao = 60;
+
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody>();
+		rb.velocity = new Vector3(Random.value, Random.value, Random.value);
+		controle = FindObjectOfType<Controle>();
+
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		 
+
+		Vector3 separacao = Separar();
+		Vector3 alinhar = Alinhar();
+		Vector3 coesao = Coesao();
+
+		separacao =  controle.ForcaSeparacao * separacao.normalized ;
+		alinhar =  controle.ForcaAlinhar * alinhar.normalized;
+		coesao = controle.ForcaCoesao * coesao.normalized;
+
+		rb.AddForce(alinhar);
+		rb.AddForce(separacao);
+		rb.AddForce(coesao);
+
+		rb.velocity = Vector3.ClampMagnitude(rb.velocity, maxSpeed);
+
+		Rotate(rb.velocity);
+		Borders();
+	}
+
+	Vector3 Coesao()
+	{
+		Vector3 soma = Vector3.zero;
+		int vizinhos = 0;
+		for (int i = 0; i < Controle.birds.Count; i++)
+		{
+			GameObject vizinho = Controle.birds[i] as GameObject;
+
+			Vector3 direcao =  vizinho.transform.position - transform.position;
+			if (direcao.magnitude < controle.DistanciaCoesao && direcao.magnitude > 0)
+			{
+
+				float angulo = Vector3.Angle(rb.velocity, direcao);
+				
+				if (angulo < AnguloVisao)
+				{
+					 				 
+					soma += vizinho.transform.position;
+					vizinhos++;
+
+				}
+			}
+			 
+
+		}
+
+		if (vizinhos > 0)
+		{
+			soma = soma / vizinhos;
+			 
+			 
+			return Perseguir(soma);
+		}
+		else
+		{
+			return Vector3.zero;
+		}
+
+	}
+	Vector3 Alinhar()
+	{
+		Vector3 soma = Vector3.zero;
+		int vizinhos = 0;
+		for (int i = 0; i < Controle.birds.Count; i++)
+		{
+			GameObject vizinho = Controle.birds[i] as GameObject;
+
+			Vector3 direcao = vizinho.transform.position - transform.position ;
+			
+				
+			if (direcao.magnitude < controle.DistanciaAlinhar && direcao.magnitude > 0)
+			{
+				float angulo = Vector3.Angle(rb.velocity, direcao);
+
+				if (angulo < AnguloVisao) {
+					 
+					soma += vizinho.GetComponent<Rigidbody>().velocity;
+						vizinhos++;
+
+				}
+
+			}
+
+			
+		}
+
+		if(vizinhos > 0)
+		{
+			soma = soma / vizinhos;
+			soma = soma.normalized* maxSpeed;
+			Vector3 perseguir = soma - rb.velocity;
+			Vector3.ClampMagnitude(perseguir, maxSpeed);		
+			return Perseguir(perseguir);
+		}
+		else
+		{
+			return Vector3.zero;
+		}
+
+
+
+	}
+
+	void Borders()
+	{
+		Vector2 screenPosition = Camera.main.WorldToScreenPoint(transform.position);
+	 
+		 
+
+		if (screenPosition.y > Screen.height)
+		{
+			transform.position = new Vector3(transform.position.x, -transform.position.y);
+		}
+
+		if (screenPosition.y < 0)
+		{
+			transform.position = new Vector3(transform.position.x, -transform.position.y);
+		}
+
+		if (screenPosition.x > Screen.width)
+		{
+			transform.position = new Vector3(-transform.position.x, transform.position.y);
+		}
+
+		if (screenPosition.x < 0)
+		{
+			transform.position = new Vector3(-transform.position.x, transform.position.y);
+		}
+	}
+
+	Vector3 Separar()
+	{
+		Vector3 force = Vector3.zero;
+		for(int i = 0; i < Controle.birds.Count; i++)
+		{
+			GameObject vizinho = Controle.birds[i] as GameObject;
+
+			 
+				Vector3 direcao = transform.position - vizinho.transform.position;
+				if (direcao.magnitude < controle.DistanciaSeparacao && direcao.magnitude > 0)
+				{
+
+					force += direcao;
+				}
+			 
+			
+		}
+
+		return force;
+	}
+
+	Vector3 Perseguir(Vector3 target)
+	{
+
+		if(target.magnitude <= 0)
+		{
+			return Vector3.zero;
+		}
+		Vector3 disired = target - transform.position;		 
+		 
+		disired = disired.normalized * maxSpeed;
+		Vector3 steer = disired - rb.velocity;
+		return steer;
+		 
+		 
+			
+		
+	}
+
+	void Rotate(Vector3 direcao)
+	{
+
+		//var dir = direcao - transform.position;
+		var dir = direcao ;
+		var angle = 180 + Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
+		transform.rotation = Quaternion.Lerp(transform.rotation, Quaternion.AngleAxis(angle, Vector3.forward),0.2f)	;
+
+	}
+
+	 
+}
---
+++ Assets/Scripts/Controle.cs
@@ -0,0 +1,143 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Controle : MonoBehaviour {
+
+	public static ArrayList birds;
+	public int passaros = 10;
+	public GameObject birdModel;
+
+
+	private float forcaSeparacao = 3;
+
+	private float forcaCoesao = 1;
+
+	private float forcaAlinhar = 1;
+
+	private float distanciaSeparacao = 2;
+
+	private float distanciaCoesao = 5;
+
+	private float distanciaAlinhar = 1;
+
+	public   float ForcaSeparacao { get { return forcaSeparacao; } }
+	 
+	public   float ForcaCoesao { get { return forcaCoesao; } }
+
+	public   float ForcaAlinhar { get { return forcaAlinhar; } }
+
+	public   float DistanciaSeparacao { get { return distanciaSeparacao; } } 
+
+	public   float DistanciaCoesao { get { return distanciaCoesao; } }
+
+	public    float DistanciaAlinhar { get { return distanciaAlinhar; } }
+
+	public Scrollbar ScrollForcaSeparacao  ;
+
+	public Scrollbar ScrollForcaCoesao;
+
+	public Scrollbar ScrollForcaAlinhar;
+
+	public Scrollbar ScrollDistanciaSeparacao;
+
+	public Scrollbar ScrollDistanciaCoesao;
+
+	public Scrollbar ScrollDistanciaAlinhar;
+
+	public Text TForcaSeparacao;
+	public Text TForcaCoesao;
+	public Text TForcaAlinhar;
+	public Text TDistanciaSeparacao;
+	public Text TDistanciaCoesao;
+	public Text TDistanciaAlinhar;
+
+
+	// Use this for initialization
+	void Start () {
+		birds = new ArrayList();
+		
+		for(int i = 0; i < passaros; i++)
+		{
+
+			float Y = Random.Range
+				(Camera.main.ScreenToWorldPoint(new Vector2(0, 0)).y, Camera.main.ScreenToWorldPoint(new Vector2(0, Screen.height)).y);
+			float X = Random.Range
+				(Camera.main.ScreenToWorldPoint(new Vector2(0, 0)).x, Camera.main.ScreenToWorldPoint(new Vector2(Screen.width, 0)).x);
+
+			birds.Add(Instantiate(birdModel, new Vector3(X,Y,0), Quaternion.identity));
+
+
+
+		}
+
+		ScrollForcaSeparacao.onValueChanged.AddListener(delegate { MudarForcaSeparacao(); });
+		ScrollForcaCoesao.onValueChanged.AddListener(delegate { MudarForcaCoesao(); });
+		ScrollForcaAlinhar.onValueChanged.AddListener(delegate { MudarForcaAlinhar(); });
+		ScrollDistanciaSeparacao.onValueChanged.AddListener(delegate { MudarDistanciaSeparacao(); });
+		ScrollDistanciaCoesao.onValueChanged.AddListener(delegate { MudarDistanciaCoesao(); });
+		ScrollDistanciaAlinhar.onValueChanged.AddListener(delegate { MudarDistanciaAlinhar(); });
+
+		MudarForcaSeparacao();
+		MudarForcaCoesao();
+		MudarForcaAlinhar();
+		MudarDistanciaSeparacao();
+		MudarDistanciaCoesao();
+		MudarDistanciaAlinhar();
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+
+ 
+
+
+	}
+
+	public void MudarForcaSeparacao()
+	{
+		forcaSeparacao = Mathf.Lerp(0, 20, ScrollForcaSeparacao.GetComponent<Scrollbar>().value);
+
+		TForcaSeparacao.text = "Força Separação:" + forcaSeparacao;
+	
+
+	}
+	public void MudarForcaCoesao()
+	{
+		forcaCoesao = Mathf.Lerp(0, 20, ScrollForcaCoesao.GetComponent<Scrollbar>().value);
+		TForcaCoesao.text = "Força Coesão:" + forcaCoesao;
+
+	}
+	public void MudarForcaAlinhar()
+	{
+		forcaAlinhar = Mathf.Lerp(0, 20, ScrollForcaAlinhar.GetComponent<Scrollbar>().value);
+		 TForcaAlinhar.text = "Força Alinhar:" + forcaAlinhar;
+	
+
+}
+	public void MudarDistanciaSeparacao()
+	{
+		distanciaSeparacao = Mathf.Lerp(0, 0.5f, ScrollDistanciaSeparacao.GetComponent<Scrollbar>().value);
+		TDistanciaSeparacao.text = "Distância Separação:" + distanciaSeparacao;
+
+
+}
+	public void MudarDistanciaCoesao()
+	{
+		distanciaCoesao = Mathf.Lerp(0, 5, ScrollDistanciaCoesao.GetComponent<Scrollbar>().value);
+		TDistanciaCoesao.text = "Distância Coesão:" + distanciaCoesao;
+
+
+ 
+	
+
+}
+	public void MudarDistanciaAlinhar()
+	{
+		distanciaAlinhar = Mathf.Lerp(0, 5, ScrollDistanciaAlinhar.GetComponent<Scrollbar>().value);
+		 TDistanciaAlinhar.text = "Distância Alinhar:" + distanciaAlinhar;
+	}
+
+}
